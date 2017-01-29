cd001f6ec891a303afa2bd830efaccaefbf16712
Ferdi Elik <elik.ferdi@gmail.com>
2017-01-30 00:08:01 +0300

    -

--- src/main/java/com/ferdielik/configuration/HibernateConfiguration.java
+++ src/main/java/com/ferdielik/configuration/HibernateConfiguration.java
@@ -51,6 +51,9 @@ public class HibernateConfiguration
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.connection.CharSet", environment.getRequiredProperty("hibernate.connection.CharSet"));
+        properties.put("hibernate.connection.characterEncoding", environment.getRequiredProperty("hibernate.connection.characterEncoding"));
+        properties.put("hibernate.connection.useUnicode", environment.getRequiredProperty("hibernate.connection.useUnicode"));
         return properties;
     }
 
